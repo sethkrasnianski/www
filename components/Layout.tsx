cee7c6559f5e72f6664f162df5858fa627e9f1f7
Seth Krasnianski <seth@mojotech.com>
2022-09-21 05:00:45 -0400

    chore: Extract app layout to dedicated file

---
+++ components/Layout.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import Script from "next/script";
+import Footer from "components/Footer";
+import Header from "components/Header";
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <title>Software Engineer | Seth J Krasnianski</title>
+        <meta
+          name="description"
+          content="Developer of software currently working with the fine folks at MojoTech."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://use.typekit.net/qka6xnx.css" />
+      </Head>
+      <Script src="https://use.fonticons.com/13897413.js"></Script>
+
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,31 +2,13 @@ import "styles/globals.css";
 import "styles/Home.css";
 
 import type { AppProps } from "next/app";
-import Head from "next/head";
-import Script from "next/script";
-import Header from "components/Header";
-import Footer from "components/Footer";
+import Layout from "components/Layout";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <title>Software Engineer | Seth J Krasnianski</title>
-        <meta name="description" content="" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://use.typekit.net/qka6xnx.css" />
-      </Head>
-      <Script src="https://use.fonticons.com/13897413.js"></Script>
-
-      <Header />
-
-      <main>
-        <Component {...pageProps} />
-      </main>
-
-      <Footer />
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
