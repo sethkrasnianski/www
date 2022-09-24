037eeaf2c61a3f3592853d793484e8336e6cd681
Seth Krasnianski <seth@mojotech.com>
2022-09-24 19:44:21 -0400

    feature: Add dark mode capability
    Eventually a button will be available to manually select. But this
    provides the foundation for enabling browsers to choose the system
    default.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,32 @@ import "styles/globals.css";
 
 import type { AppProps } from "next/app";
 import Layout from "components/Layout";
+import { useEffect } from "react";
 
 function App({ Component, pageProps }: AppProps) {
+  const setDarkTheme = (dark: boolean) => {
+    if (dark) {
+      document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.removeAttribute("data-theme");
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window === "undefined") {
+      return;
+    }
+
+    const handleChangeTheme = (e: MediaQueryListEvent) =>
+      setDarkTheme(e.matches);
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    setDarkTheme(mediaQuery.matches);
+
+    mediaQuery.addEventListener("change", handleChangeTheme);
+    return () => mediaQuery.removeEventListener("change", handleChangeTheme);
+  }, []);
+
   return (
     <Layout>
       <Component {...pageProps} />
